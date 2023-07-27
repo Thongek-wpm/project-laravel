8e8a6e9c061626b6ca4f750a7162dbe0dbf007d4
Thongek Wogponom <thongek.wpm@gmail.com>
2023-07-27 11:42:31 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,35 +2,34 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         User Information Status , {{Auth::user()->name}}
-        <b class="float-end">Number of Online Users {{count($user)}} Person</b>
+        <b class="float-end">จำนวนผู้ใช้ระบบ <span>{{count($users)}}</span> คน</b>
         </h2>
     </x-slot>
-
     <div class="py-12">
-       <div class= "container">
-        <div class="row">
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Date register</th>
-    </tr>
-  </thead>
-  <tbody>
-  @php($i=1)
-  @foreach ($user as $row )
-  <tr>
-    <th>{{$i++}}</th>
-    <td>{{$row -> name}}</td>
-    <td>{{$row -> email}}</td>
-    <td>{{$row -> created_at->diffForHumans()}}</td>
-  </tr>
-  @endforeach
-  </tbody>
-</table>
+        <div class="container">
+            <div class="row">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                    <th scope="col">ลำดับ</th>
+                    <th scope="col">ชื่อ</th>
+                    <th scope="col">อีเมล</th>
+                    <th scope="col">เริ่มใช้งานระบบ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php($i=1)
+                    @foreach($users as $row)
+                    <tr>
+                        <th>{{$i++}}</th>
+                        <td>{{$row->name}}</td>
+                        <td>{{$row->email}}</td>
+                        <td>{{Carbon\Carbon::parse($row->created_at)->diffForHumans()}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                </table>
+            </div>
         </div>
-       </div>
     </div>
 </x-app-layout>
