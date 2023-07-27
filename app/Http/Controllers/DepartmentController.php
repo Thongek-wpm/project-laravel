cd44c7b44ef525ab551dea6678bdf310b67bc5e4
Thongek Wogponom <thongek.wpm@gmail.com>
2023-07-27 11:12:55 +0700

    commit

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -11,9 +11,11 @@ class DepartmentController extends Controller
 {
     //
     public function index(){
-        $departments=Department::all();
-        return view('admin.department.index',compact('departments'));
+        $departments=Department::paginate(5);
+        $trashDepartments = Department::onlyTrashed()->paginate(3);
+        return view('admin.department.index',compact('departments','trashDepartments'));
     }
+
     public function store(Request $request){
         //ตรวจสอบข้อมูล
         $request->validate(
@@ -34,5 +36,26 @@ class DepartmentController extends Controller
         DB::table('departments')->insert($data);
 
         return redirect()->back()->with('success',"บันทึกข้อมูลเรียบร้อย");
+    } public function edit($id){
+        $department = Department::find($id);
+        return view('admin.department.edit',compact('department'));
+    }
+    public function update(Request $request , $id){
+        //ตรวจสอบข้อมูล
+        $request->validate(
+            [
+                'department_name'=>'required|unique:departments|max:255'
+            ],
+            [
+                'department_name.required'=>"กรุณาป้อนชื่อแผนกด้วยครับ",
+                'department_name.max' => "ห้ามป้อนเกิน 255 ตัวอักษร",
+                'department_name.unique'=>"มีข้อมูลชื่อแผนกนี้ในฐานข้อมูลแล้ว"
+            ]
+        );
+        $update = Department::find($id)->update([
+            'department_name'=>$request->department_name,
+            'user_id'=>Auth::user()->id
+        ]);
+        return redirect()->route('department')->with('success',"อัพเดตข้อมูลเรียบร้อย");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Models\Department;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\DepartmentController;
-
+use App\Http\Controllers\ServiceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,17 +19,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        $user=user::all();
-        return view('dashboard',compact('user'));
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    $users=DB::table('users')->get();
+    return view('dashboard',compact('users'));
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    //Department
+    Route::get('/department/all',[DepartmentController::class,'index'])->name('department');
+    Route::post('/department/add',[DepartmentController::class,'store'])->name('addDepartment');
+    Route::get('/department/edit/{id}',[DepartmentController::class,'edit']);
+    Route::post('/department/update/{id}',[DepartmentController::class,'update']);
+
+    //SoftDelete
+    Route::get('/department/softdelete/{id}',[DepartmentController::class,'softdelete']);
+    Route::get('/department/restore/{id}',[DepartmentController::class,'restore']);
+    Route::get('/department/delete/{id}',[DepartmentController::class,'delete']);
+
+    //Service
+    Route::get('/service/all',[ServiceController::class,'index'])->name('services');
+    Route::post('/service/add',[ServiceController::class,'store'])->name('addService');
+
+    Route::get('/service/edit/{id}',[ServiceController::class,'edit']);
+    Route::post('/service/update/{id}',[ServiceController::class,'update']);
+    Route::get('/service/delete/{id}',[ServiceController::class,'delete']);
 });
-Route::get('department/all',[DepartmentController::class,'index'])
-->name('department');
-Route::post('/Department/add',[DepartmentController::class,'store'])
-->name('addDepartment');
